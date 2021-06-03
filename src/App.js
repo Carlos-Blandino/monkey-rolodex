f11a10033712315f9a31b05882e7983a1d2dfb8b
Carlos Blandino <cablandino66@gmail.com>
2021-06-03 15:47:15 -0700

    adding image #10 and removing splice

--- src/App.js
+++ src/App.js
@@ -1,37 +1,47 @@
-import React, { Component } from 'react';
-import { Layout } from './components/card-list/Layout'
-import './App.css';
+import React, { Component } from "react";
+import { Layout } from "./components/card-list/Layout";
+import "./App.css";
 
 class App extends Component {
   constructor() {
     super();
     this.state = {
-      monkeys : [],
-        searchField : "",
-    }
+      monkeys: [],
+      searchField: "",
+    };
   }
 
   componentDidMount() {
-    fetch('https://jsonplaceholder.typicode.com/users')
-    .then( response => response.json())
-    .then(users => this.setState({monkeys: users}))
+    fetch("https://jsonplaceholder.typicode.com/users")
+      .then((response) => response.json())
+      .then((users) => this.setState({ monkeys: users }));
   }
   onChangeHandler = async (e) => {
-      e.preventDefault()
-       await this.setState({searchField:e.target.value})
-    }
+    e.preventDefault();
+    await this.setState({ searchField: e.target.value });
+  };
 
-  render () {
+  render() {
+    const { monkeys, searchField } = this.state;
+    const foundMonkey = monkeys.filter((monkey) => {
+      return monkey.name.toLowerCase().includes(searchField.toLowerCase());
+    });
+
     return (
-    <div className="App">
+      <div className="App">
         <form>
-            <input type="search" placeholder='search for a monkey' name='monkey-search' id='monkey-search' onChange={(e)=> this.onChangeHandler(e) } />
+          <input
+            type="search"
+            placeholder="search for a monkey"
+            name="monkey-search"
+            id="monkey-search"
+            onChange={(e) => this.onChangeHandler(e)}
+          />
         </form>
-    <Layout monkeys={this.state.monkeys}/>
-    </div>
-  );
-}
-
+        <Layout monkeys={foundMonkey} />
+      </div>
+    );
+  }
 }
 
 export default App;

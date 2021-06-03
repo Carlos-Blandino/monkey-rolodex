6214dc6d188ccaf2c575bc7dc3175d4a4b3b1e68
Carlos Blandino <cablandino66@gmail.com>
2021-06-03 12:00:05 -0700

    adding images to cards

--- src/components/card/Card.js
+++ src/components/card/Card.js
@@ -6,7 +6,7 @@ export const Card =({monkey}) => {
 
   return  (
         <div className={"card-container"}>
-            <img alt="monkey" src={""} width={"180px"}/>
+            <img alt="monkey" src={`https://github.com/Carlos-Blandino/monkey-rolodex/blob/main/src/components/card/images/${monkey.id}.jpg?raw=true`} width={"200px"}/>
             <h1>
                 {monkey.name}
             </h1>
